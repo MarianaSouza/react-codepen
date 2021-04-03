4c30d622d09daf2bd49c7f49e6cc6837d100d001
MarianaSouza <mariana.desouza@pixelunion.net>
2021-04-02 17:01:33 -0700

    add Editor component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import Editor from "./Editor";
 
 function App() {
 	return (
-		<div>
-			<h1>Hello World!</h1>
-		</div>
+		<>
+			<div className="pane top-pane">
+				<Editor />
+				<Editor />
+				<Editor />
+			</div>
+			<div className="pane">
+				<iframe
+					title="output"
+					sandbox="allow-scripts"
+					frameBorder="0"
+					width="100%"
+					height="100%"
+				/>
+			</div>
+		</>
 	);
 }
 
